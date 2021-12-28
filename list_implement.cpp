89ca036f9d39c544a7abf190ef5fd834db18d961
Paul Lachaud <paul.lachaud@outlook.com>
2021-12-28 16:26:32 -0500

    use templates for generic list

--- list_implement.cpp
+++ list_implement.cpp
@@ -1,18 +1,67 @@
 #include <iostream>
+#include <vector>
 
-
+template <typename T>
 struct ListNode
 {
     ListNode* next;
-    int key;
+    T key;
 
-    ListNode(int key) : key(key) {};
+    ListNode() {}
+    ListNode(T key) : key(key) {}
 };
+
+template <typename T>
+struct List {
+    ListNode<T>* head;
+    ListNode<T>* tail;
+
+    List() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert(int val)
+    {
+        ListNode<T>* tmp = new ListNode<T>(val);
+        if (head == nullptr)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else {
+            tail->next = tmp;
+            tail = tmp;
+        }
+    }
+
+    void display(std::ostream& os)
+    {
+        ListNode<T>* node = head;
+        while (node != nullptr)
+        {
+            os << node->key << " ";
+            node = node->next;
+        }
+        os << std::endl;
+    }
+};
+
+
 
 int main()
 {
-    auto head = ListNode(5);  
-    head.next = new ListNode(6);
+    List<int> list = List<int>();
+    list.insert(5);
+    list.display(std::cout);
 
-    std::cout << head.key << std::endl;
+    list.insert(12);
+    list.insert(34);
+    list.insert(3);
+    list.insert(22);
+    list.insert(5);
+    list.insert(4);
+
+    list.display(std::cout);
+
 }
